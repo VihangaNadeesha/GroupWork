3b2422e194bce55acfd18e519b7881eea03504d1
Sheraperera <87012378+Sheraperera@users.noreply.github.com>
2021-07-06 15:53:39 +0530

    Update Player.java

--- Player.java
+++ Player.java
@@ -5,17 +5,33 @@ public class Player {
 	private String playerType;
 	private double playerStats;
 	
-	public static void main(String[] args);
-	public Player (String plyName, String plyAge, String plyType, double plyStats ){
-		playerName = plyName;
-		playerAge = plyAge;
-		playerType = plyType;
-		playerStats = plyStats;
+	public Player (String playerName, String playerAge, String playerType, double playerStats ){
+		this.playerName = playerName;
+		this.playerAge = playerAge;
+		this.playerType = playerType;
+		this.playerStats = playerStats;
 	}
-		public void displayPlayerDetails () {
-			System.out.println("Player Name: " + playerName);
-			System.out.println("Player Age: " + playerAge);
-			System.out.println("Player Type: " + playerType);
-			System.out.println("Player Stats: " + playerStats);
-		}
+	void Addplayers (){
+		System.out.println("Enter players Name ");
+		playerName = input.nextline;
+		System.out.println("Enter players age ");
+		playerage = input.nextInt;
+		System.out.println("Enter players Type ");
+		playerType = input.nextline;
+		System.out.println("Enter playersstatus  ");
+		playerStats = input.nextline;
+	
+	
+	}
+	
+	public void displayPlayerDetails () {
+		System.out.println("Player Name: " + playerName);
+		System.out.println("Player Age: " + playerAge);
+		System.out.println("Player Type: " + playerType);
+		System.out.println("Player Stats: " + playerStats);
+	}
+	
+	void average () { 
+		
+	}
 }
